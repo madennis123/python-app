4f4fec93a08a18f2589e85d2803e05ae2155d7a5
madennis123 <madennis123@yahoo.co.uk>
2017-09-07 16:17:32 +0100

    changed the font

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! How are yoiu doing today?" 
+    return "<h1> Hello World! How are yoiu doing today? </h1>" 
 
 if __name__ == "__main__":
     application.run()
